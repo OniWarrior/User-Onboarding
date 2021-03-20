e1251552db200ad67e526539fba551a340f1f6f5
Stephen Aranda <aranda.stephen88@gmail.com>
2021-03-19 19:48:01 -0500

    Completed project. Finished tests to check for checkbox input, submission of data and form validation for empty input

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -3,32 +3,58 @@ describe('Form App',()=>{
     const nameInput = () => cy.get('.inputs > :nth-child(2) > input')
     const emailInput = () => cy.get(':nth-child(3) > input')
     const passwordInput = () => cy.get(':nth-child(4) > input')
-    
+    const checkBoxInput = () => cy.get('.checkboxes > label > input')
+    const testName = 'John'
+    const testEmail = 'john@john.com'
+    const testPassword= '123456789456'
+    const testCheckBox = 'on'
+    const submitButton = () => cy.get('button')
 
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
     })
 
     describe('Filling out input', () => {
-        it('can type in the inputs and cancel', () => {
+        it('can type in the inputs', () => {
             nameInput()
-            .should('have.value', '')
-            .type(nameInput)
-            .shoud('have.value', nameInput)
+            .should('have.value', '')   // ensures that this is empty prior to inputting values
+            .type(testName)         
+            .should('have.value', testName)
            
             emailInput()
-            .should('have.value', '')
-            .type(emailInput)
-            .shoud('have.value', emailInput)
+            .should('have.value', '')  // ensures that this is empty prior to inputting values
+            .type(testEmail)          
+            .should('have.value', testEmail)
 
             passwordInput()
-            .should('have.value', '')
-            .type(passwordInput)
-            .shoud('have.value',passwordInput)
-      
+            .should('have.value', '')  // ensures that this is empty prior to inputting values
+            .type(testPassword)          
+            .should('have.value',testPassword)
+
+            checkBoxInput()
+            .should('have.value', 'off')            // ensures that this is unchecked prior to inputting value
+            .type(testCheckBox)
+            .should('have.value',testCheckBox)      
           
           })
     })
+
+    describe('Submitting user', () => {
+        it('can type in the inputs and submit', () => {
+            nameInput().type(testName)
+            emailInput().type(testEmail)
+            passwordInput().type(testPassword)           
+    
+           submitButton().click()
+    
+           const newUser = () => cy.contains(testName)
+    
+           newUser().should('exist')
+    
+           //cleanup
+           newUser().next().next().click()
+        })
+      })
 
 
 
